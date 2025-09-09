f8db94eb93c0e73b8a37ba7b05ec2283e0e7253b
sunyeog <shtnsgur0118@naver.com>
2025-09-09 14:15:24 +0000

    1

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,10 +1,12 @@
 #include "push_swap.h"
 
-typedef struct s_node
+typedef struct s_node t_node;
+
+struct s_node
 {
     int data;
     t_node *next;
-} t_node;
+} ;
 
 typedef struct s_point
 {
@@ -14,23 +16,36 @@ typedef struct s_point
     int     len;
 } t_point;
 
-void    l_init(t_point *p)
+t_point *l_init(void)//header 생성 및 초기화
 {
+    t_point *p = (t_point *)malloc(sizeof(t_point));
+
     p->head = (t_node   *)malloc(sizeof(t_node));
     p->head->next = NULL;
     p->len = 0;
+    return (p);
 }
 
-void    Finsert(t_point *p, int d)
+void    Finsert(t_point *p, int d)//첫 노드생성 후 값 삽입
 {
     t_node  *new = (t_node  *)malloc(sizeof(t_node));
     new->data = d;
-    p->next = new;
+    p->head->next = new;
     new->next = NULL;
 }
 
 int main(int ac, char **av)
 {
+    t_point *p;
+    int value;
+
+    p = l_init();
+    value = ft_atoi(av[1]);
+    Finsert(p, value);
+    
+    ft_printf("%d\n", p->head->next->data);
+    ft_printf("%d\n", ac);
+    return 0;
 
     // av[0][0] = 0;
     // len = ac;
