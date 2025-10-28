1de0e021e481debe119083daa0b5a01717a53d65
sunyeog <shtnsgur0118@naver.com>
2025-10-28 13:21:32 +0000

    1

---
+++ else/최댓값.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 0;
+    int i = 0;
+    int s_i = 0;
+    while (i < 10)
+    {
+        scanf("%d", &a);
+        if (a > b)
+        {
+            b = a;
+            s_i = i;
+        }
+        i++;
+    }
+    printf("%d\n%d\n", b, s_i);
+    return (0);
+    
+}
