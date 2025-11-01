61bcf1c9888635c0a9eda108e34169f3441019ca
sunyeog <shtnsgur0118@naver.com>
2025-11-01 14:31:04 +0000

    1

--- else/체스판 다시 칠하기.c
+++ else/체스판 다시 칠하기.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int check_col(char **chess)
+int cmp_str(char **chess, int a, int b)
 {
+    char num1[9] = BW";
+    char num2[9] = "WBWBWBWB"BWBWBW";
     int i = 0, j = 0;
-    
+    chess[b + i][a + j]
 }
 
-int check_row(char **chess)
+int if_B(char *chess, int a, int b)
 {
-    int i = 0, j = 0;
-    
+    char num1[9] = "BWBWBWBW";
+    char num2[9] = "WBWBWBWB";
+
+    int i = 0;
+    while (i < 8)
+    {
+        chess[i]
+    }
 }
+
+
 
 int return_lowest(char **chess, int a, int b)
 {
@@ -35,13 +45,13 @@ int main()
     int i = 0;
     
     scanf("%d %d", &a, &b);
-    char **chess = (char **)malloc(sizeof(char *) * b + 1);
-    chess[b + 1] = NULL;
+    char **chess = (char **)malloc(sizeof(char *) * (b + 1));
+    chess[b] = NULL;
     char *str;
     while (i < b)
     {
-        str = (char *)malloc(sizeof(char) * a + 1);
-        str[a + 1] = '\0';
+        str = (char *)malloc(sizeof(char) * (a + 1));
+        str[a] = '\0';
         scanf("%s", str);
         chess[i] = str;
         i++;
