83fbc419c14f2c035d08f73b5a5bbeeecc0f7f4d
sunyeog <shtnsgur0118@naver.com>
2025-11-17 14:56:32 +0000

    1

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -19,7 +19,7 @@ t_point *l_init(void)// header 생성 및 초기화
     t_point *p = (t_point *)malloc(sizeof(t_point));
     
     p->top = (t_node   *)malloc(sizeof(t_node));
-    p->top->next = NULL;
+    p->top = NULL;
     p->len = 0;
     return (p);
 }
@@ -28,7 +28,6 @@ void  pop(t_point *p)
 {
     t_node *temp = p->top;
     p->top = p->top->next;
-    temp->next = temp;
     free(temp);
 }
 
@@ -44,9 +43,8 @@ void    push(t_point *p, int d)//첫 노드 이후 값들 연결
 {
     t_node  *new = (t_node  *)malloc(sizeof(t_node));
     new->data = d;
-    p->top->next = new;
+    new->next = p->top;
     p->top = new;
-    new->next = NULL;
 }
 
 int main(int ac, char **av)
@@ -58,8 +56,29 @@ int main(int ac, char **av)
     value = ft_atoi(av[ac - 1]);
     Finsert(p, value);
 
+    int i = 0;
+    while (i < ac - 2)
+    {
+        value = ft_atoi(av[ac - 2 - i]);
+        push(p, value);
+        i++;
+    }
+    i = 0;
+    while (i < ac - 1)
+    {
+        ft_printf("%d\n" , p->top->data);
+        p->top = p->top->next;
+        i++;
+    }
+    pop(p);
+    i = 0;
     t_node *cur = p->top;
-    ft_printf("%d\n" ,cur->data);
+    while (i < ac - 2)
+    {
+        ft_printf("%d\n" , cur->data);
+        cur = cur->next;
+        i++;
+    }
 
     // ft_printf("%d\n", p->top->data);
     // ft_printf("%d\n", p->cur->data);
