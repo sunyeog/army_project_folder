d26cffb41e1c2d59ad7dd3a68f3f2ed20f598c49
sunyeog <shtnsgur0118@naver.com>
2025-11-22 14:35:29 +0000

    1

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -11,16 +11,16 @@ struct s_node
 typedef struct s_point
 {
     t_node  *top;
-    int     len;
+    int     size;
 } t_point;
 
 t_point *l_init(void)// header 생성 및 초기화
 {
     t_point *p = (t_point *)malloc(sizeof(t_point));
     
-    p->top = (t_node   *)malloc(sizeof(t_node));
+    p->top = (t_node *)malloc(sizeof(t_node));
     p->top->next = NULL;
-    p->len = 0;
+    p->size = 0;
     return (p);
 }
 
@@ -29,11 +29,13 @@ void  pop(t_point *p)
     t_node *temp = p->top;
     p->top = p->top->next;
     free(temp);
+    p->size--;
 }
 
 void    F_push(t_point *p, int d)//첫 노드생성 후 값 삽입
 {
     p->top->data = d;
+    p->size++;
 }
 
 void    push(t_point *p, int d)//첫 노드 이후 값들 연결
@@ -42,15 +44,17 @@ void    push(t_point *p, int d)//첫 노드 이후 값들 연결
     new->data = d;
     new->next = p->top;
     p->top = new;
+    p->size++;
 }
 
 int main(int ac, char **av)
 {
     t_point *stack_A;
-    //t_point *stack_B;
+    // t_point *stack_B;
     int value;
 
     stack_A = l_init();
+    // stack_B = l_init();
     value = ft_atoi(av[ac - 1]);
     F_push(stack_A, value);
 
@@ -61,6 +65,10 @@ int main(int ac, char **av)
         push(stack_A, value);
         i++;
     }
+
+
+
+    
     i = 0;
     t_node *cur = stack_A->top;
     while (i < ac - 1)
@@ -69,6 +77,7 @@ int main(int ac, char **av)
         cur = cur->next;
         i++;
     }
+    ft_printf("size : %d\n", stack_A->size);
     pop(stack_A);
     ft_printf("-------------------\n");
     i = 0;
@@ -79,6 +88,7 @@ int main(int ac, char **av)
         cur = cur->next;
         i++;
     }
+    ft_printf("size : %d\n", stack_A->size);
     pop(stack_A);
     ft_printf("-------------------\n");
     i = 0;
@@ -89,6 +99,7 @@ int main(int ac, char **av)
         cur = cur->next;
         i++;
     }
+    ft_printf("size : %d\n", stack_A->size);
 
     // ft_printf("%d\n", p->top->data);
     // ft_printf("%d\n", p->cur->data);
---
+++ push_swap/push_swap_utils.c
@@ -0,0 +1,4 @@
+void sa(t_point *a, t_point *b)
+{
+    
+}
