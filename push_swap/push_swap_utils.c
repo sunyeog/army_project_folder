f5d40af0f741ca55fa487428e314fa28da6ff461
sunyeog <shtnsgur0118@naver.com>
2025-11-25 14:55:05 +0000

    1

--- push_swap/push_swap_utils.c
+++ push_swap/push_swap_utils.c
@@ -1,4 +1,32 @@
-void sa(t_point *a, t_point *b)
+void sa(t_point *stack_A)
 {
+    int temp;
+
+    if (stack_A->size < 2)
+        return;
+    temp = stack_A->top->data;
+    stack_A->top->data = stack_A->top->next->data;
+    stack_A->top->next->data = temp;
+}
+
+void sb(t_point *stack_B)
+{
+    int temp;
     
+    if (stack_B->size < 2)
+        return;
+    temp = stack_B->top->data;
+    stack_B->top->data = stack_B->top->next->data;
+    stack_B->top->next->data = temp;
+}
+
+void ss(t_point *stack_A, t_point *stack_B)
+{
+    sa(stack_A);
+    sb(stack_B);
+}
+
+void pa(t_point *stack_A, t_point *stack_B)
+{
+
 }
