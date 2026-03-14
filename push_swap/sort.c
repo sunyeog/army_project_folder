aab0c9ac0ff4fe459afaf7e0bffa7acfc6e53d62
sunyeog <shtnsgur0118@naver.com>
2026-03-14 11:02:54 +0000

    1

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -6,6 +6,8 @@ void    size_is_2(t_point *stack_A)
         sa(stack_A, 1);
 }
 
+//-----------------------------------------
+
 void    size_is_3(t_point *stack_A)
 {
     int top;
@@ -33,4 +35,41 @@ void    size_is_3(t_point *stack_A)
         sa(stack_A, 1);
 }
 
-int *to_index_arr(char)
+//-----------------------------------------
+
+void insert_index(t_point *stack_A, int *arr)
+{
+    t_node *stop_cur;
+    t_node *mv_cur;
+    int index;
+    int i;
+
+    i = 0;
+    stop_cur = stack_A -> top;
+    while (i < stack_A -> size)
+    {
+        index = 0;
+        mv_cur = stack_A -> top;
+        while (mv_cur != NULL)
+        {
+            if (stop_cur -> data < mv_cur -> data)
+                index++;
+            mv_cur = mv_cur -> next;
+        }
+        arr[i] = index;
+        stop_cur = stop_cur -> next;
+        i++;
+    }
+}
+
+int *to_index_arr(t_point *stack_A)
+{
+    int *arr;
+    
+    arr = (int *)malloc(sizeof(int) * stack_A -> size);
+    if (arr == NULL)
+        return (NULL);
+    insert_index(stack_A, arr);
+    
+    return (arr);
+}
