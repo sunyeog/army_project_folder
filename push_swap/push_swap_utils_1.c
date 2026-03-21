a449e0977ea3973c8fda36daef01bb1a3f2d9462
sunyeog <shtnsgur0118@naver.com>
2026-03-21 15:06:25 +0000

    1

--- push_swap/push_swap_utils_1.c
+++ push_swap/push_swap_utils_1.c
@@ -14,33 +14,33 @@
 
 void	sa(t_point *stack_A, int opt)
 {
-    int temp;
+	int temp;
 
-    if (stack_A -> size < 2)
-        return;
-    temp = stack_A -> top -> data;
-    stack_A -> top -> data = stack_A -> top -> next -> data;
-    stack_A -> top -> next -> data = temp;
-    if (opt == 1)
-        ft_printf("sa\n");
+	if (stack_A -> size < 2)
+		return;
+	temp = stack_A -> top -> data;
+	stack_A -> top -> data = stack_A -> top -> next -> data;
+	stack_A -> top -> next -> data = temp;
+	if (opt == 1)
+		ft_printf("sa\n");
 }
 
 void	sb(t_point *stack_B, int opt)
 {
-    int temp;
-    
-    if (stack_B -> size < 2)
-        return;
-    temp = stack_B -> top -> data;
-    stack_B -> top -> data = stack_B -> top -> next -> data;
-    stack_B -> top -> next -> data = temp;
-    if (opt == 1)
-        ft_printf("sb\n");
+	int temp;
+
+	if (stack_B -> size < 2)
+		return;
+	temp = stack_B -> top -> data;
+	stack_B -> top -> data = stack_B -> top -> next -> data;
+	stack_B -> top -> next -> data = temp;
+	if (opt == 1)
+		ft_printf("sb\n");
 }
 
 void	ss(t_point *stack_A, t_point *stack_B)
 {
-    sa(stack_A, 0);
-    sb(stack_B, 0);
-    ft_printf("ss\n");
+	sa(stack_A, 0);
+	sb(stack_B, 0);
+	ft_printf("ss\n");
 }
