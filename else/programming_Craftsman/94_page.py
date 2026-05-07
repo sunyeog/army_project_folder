7bc11f13b6bc3bbdad3ef30f806e5a9ecdf41abe
sunyeog <shtnsgur0118@naver.com>
2026-05-07 13:31:57 +0000

    1

---
+++ else/programming_Craftsman/94_page.py
@@ -0,0 +1,9 @@
+x, y = input('입력 : ').split('-')
+a = [ 'abc123', 'def456', 'ghi789' ]
+a.append(x)
+a.append(y)
+a.remove('def456')
+
+print(a[1][-3:], a[2][:-3], sep = ',')
+for i int range(3, 6):
+    print(i, end = ' ')
