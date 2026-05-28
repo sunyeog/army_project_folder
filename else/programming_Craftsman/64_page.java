8c1e2fc1e9916451d0dfd6bd745887013173e20f
sunyeog <shtnsgur0118@naver.com>
2026-05-28 08:17:39 +0000

    1

---
+++ else/programming_Craftsman/64_page.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int w = 3, x = 4, y = 3, z = 5;
+        w = y + z;
+        if (7 ==y ^ z != w)
+            System.out.println(w) ;
+        else
+            System.out.println(z) ;
+    }
+}
