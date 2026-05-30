64ecf59cad221e6217d7232156317a8b9dbd42a6
sunyeog <shtnsgur0118@naver.com>
2026-05-30 08:13:13 +0000

    1

--- else/programming_Craftsman/113_page.py
+++ else/programming_Craftsman/113_page.py
@@ -1,6 +1,20 @@
 numAry = [0, 0, 0, 0, 3]
-for j in range(4, b-1, -1):
+for j in range(4, -1, -1):
     for i in range(4, j, -1):
         numAry[j] += numAry[i]
 for j in range(5):
     print(numAry[j], end=" ")
+
+
+
+
+def isprime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return 0
+        return 1
+number = 99
+cnt = 0
+for i in range(2,number + 1):
+    cnt += isprime(i)
+print(f"{number}를 넘지않는 소수는 {cnt}개입니다.")
