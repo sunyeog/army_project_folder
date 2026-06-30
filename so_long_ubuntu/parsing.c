a268acc954ce238d72f998e1c50346b97aceeb12
sunyeog <shtnsgur0118@naver.com>
2026-06-30 14:16:51 +0000

    1

--- so_long_ubuntu/parsing.c
+++ so_long_ubuntu/parsing.c
@@ -40,17 +40,72 @@ void	mk_map_arr(int fd, t_game *game)
 		free(tmp_arr);
 		tmp_arr = get_next_line(fd);
 	}
-
 	game -> map = ft_split(res_arr, '\n');
 	free(res_arr);
 }
 
-int	check_av(int ac, char **av)
+void	start_map_check(t_game *game)
+{
+	int	i;
+	int	j;
+
+	game -> cnt_e = 0;
+	game -> cnt_c = 0;
+	game -> cnt_p = 0;
+	i = 0;
+	while (game -> map[i] != NULL)
+	{
+		j = 0;
+		while (game -> map[i][j] != '\0')
+		{
+			if (game -> map[i][j] == 'E')
+				game -> cnt_e++;
+			else if (game -> map[i][j] == 'C')
+				game -> cnt_c++;
+			else if (game -> map[i][j] == 'P')
+				game -> cnt_p++;
+			j++;
+		}
+		i++;
+	}
+	if (game -> cnt_e != 1 || game -> cnt_p != 1 || game -> cnt_c == 0
+		|| game -> w < 3 || game -> h < 3)
+	{
+		ft_printf("map error\n");
+		exit(0);
+	}
+}
+
+int	check_extension(char *av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (av[i])
+	{
+		j = 0;
+		if (av[i] == '.')
+		{
+			j = i + 1;
+			if (av[j++] == 'b' && av[j++] == 'e' && av[j++] == 'r' && av[j] == '\0')
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	check_av(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		ft_printf("error\n");
-		return (0);
+		ft_printf("error\nac isn't 2\n");
+		exit(1);
 	}
-
+	if (check_extension(av[1]) == 0)
+	{
+		ft_printf("error\nextension_error\n");
+		exit(1);
+	}
 }
--- so_long_ubuntu/so_long.c
+++ so_long_ubuntu/so_long.c
@@ -247,38 +247,6 @@ void	rect_check(t_game *game)
 	}
 }
 
-void	start_map_check(t_game *game)
-{
-	int	i;
-	int	j;
-
-	game -> cnt_e = 0;
-	game -> cnt_c = 0;
-	game -> cnt_p = 0;
-	i = 0;
-	while (game -> map[i] != NULL)
-	{
-		j = 0;
-		while (game -> map[i][j] != '\0')
-		{
-			if (game -> map[i][j] == 'E')
-				game -> cnt_e++;
-			else if (game -> map[i][j] == 'C')
-				game -> cnt_c++;
-			else if (game -> map[i][j] == 'P')
-				game -> cnt_p++;
-			j++;
-		}
-		i++;
-	}
-	if (game -> cnt_e != 1 || game -> cnt_p != 1 || game -> cnt_c == 0
-		|| game -> w < 3 || game -> h < 3)
-	{
-		ft_printf("map error\n");
-		exit(0);
-	}
-}
-
 void    split_free(char **arr)
 {
         int     i;
