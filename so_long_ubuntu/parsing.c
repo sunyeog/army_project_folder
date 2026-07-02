a9dc3ca2b43ffd86d9392272b69b289204d67a93
sunyeog <shtnsgur0118@naver.com>
2026-07-02 03:27:59 +0000

    1

--- so_long_ubuntu/parsing.c
+++ so_long_ubuntu/parsing.c
@@ -59,37 +59,6 @@ void	mk_map_arr(int fd, t_game *game)
 	free(res_arr);
 }
 
-void	start_map_check(t_game *game)
-{
-	int	i;
-	int	j;
-
-	game -> cnt_e = 0;
-	game -> cnt_c = 0;
-	game -> cnt_p = 0;
-	i = 0;
-	while (game -> map[i] != NULL)
-	{
-		j = 0;
-		while (game -> map[i][j] != '\0')
-		{
-			if (game -> map[i][j] == 'E')
-				game -> cnt_e++;
-			else if (game -> map[i][j] == 'C')
-				game -> cnt_c++;
-			else if (game -> map[i][j] == 'P')
-				game -> cnt_p++;
-			j++;
-		}
-		i++;
-	}
-	if (game -> cnt_e != 1 || game -> cnt_p != 1 || game -> cnt_c == 0)
-	{
-		ft_printf("error\nmap_error\n");
-		exit(1);
-	}
-}
-
 int	check_extension(char *av)
 {
 	int	i;
